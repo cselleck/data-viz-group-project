09f316a758c6d15ea6ec684cd4a5144d163afc07
mgaleski <mpgaleski@outlook.com>
2020-06-11 13:14:57 -0500

    added matt files

---
+++ flaskapp.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+import os
+
+
+print(os.getcwd())
+keywords = []
+
+app = Flask(__name__)
+
+engine = create_engine()
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Mistruth = Base.classes.mistruth
+
+@app.route ('/api/v1.0/trump_untruths')
+
+def untruths():
+    return jsonify(data_points)
+
+@app.route("/")
+def welcome():
+    return(
+        f'Welcome to our API!<br/>'
+        f'Available Routes: <br/>'
+        f'/api/v1.0/trump_untruths (returns all untruths) <br/>'
+        f'/api/v1.0/trump_untruths/keyword returns all untruths with associated keyword) <br/>'
+        f'Keywords include: '
+    )
+@app.route('/api/v1.0/trump_mistruths/<keyword>')
+def find_keyword(keyword):
+    canonicalized = keyword.lower()
+    for word in keywords:
+        search_term = word.lower()
+        if search_term == canonicalized:
+            return jsonify(keyword)
+    return jsonify({'error': f'{keyword} not found in keywords'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
