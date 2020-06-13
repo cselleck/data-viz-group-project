baacf6faae99cddbf78096f449f98054245ee820
cselleck <57884991+cselleck@users.noreply.github.com>
2020-06-13 02:24:36 -0500

    adding code to use flask app to deploy

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from flask import Blueprint
 from flask_api import FlaskAPI
+from flask import Flask, render_template, request, redirect
 import os
 
 os.chdir('../data-viz-group-project')
@@ -22,13 +23,44 @@ def untruths():
     return api_data
 
 
-@app.route("/")
+@app.route("/api")
 def welcome():
     return(
         f'Welcome to our API!<br/>'
         f'Available Routes: <br/>'
         f'/api/v1.0/alltweets (returns all tweets as json) <br/>'
+
     )
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+
+@app.route("/bar_chart")
+def bar_chart():
+    return render_template("bar_chart.html")
+
+
+@app.route("/pie_chart")
+def pie_chart():
+    return render_template("pie_chart.html")
+
+
+@app.route("/any_chart")
+def any_chart():
+    return render_template("any_chart.html")
+
+
+@app.route("/comparison")
+def comparison():
+    return render_template("comparison.html")
+
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+
 
 
 if __name__ == '__main__':
